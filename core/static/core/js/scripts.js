acc412e9764c8c2d5360f5ee9a29720f79afd304
ju.ponce <ju.ponce@duocuc.cl>
2022-10-27 09:45:17 -0300

    avance registro

---
+++ core/static/core/js/scripts.js
@@ -0,0 +1,13 @@
+// Register Variables
+
+const userName = document.getElementById('id_username');
+
+
+// Register Functions
+
+function regValidators() {
+    if (userName === document.activeElement) {
+        console.log("activo")
+    } 
+}
+
