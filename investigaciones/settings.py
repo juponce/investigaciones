1349135667d454c9b5b772248e67ee090d4610f0
juponce <102298230+juponce@users.noreply.github.com>
2022-11-10 09:11:35 -0300

    Update settings.py

--- investigaciones/settings.py
+++ investigaciones/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'investigaciones',
         'USER': 'root',
-        'PASSWORD': 'bkfw7SrSndx8',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
